dde6f6b1e433af29e9215cbf4a1f9c589843be0c
Robin Phillips <robinphillips462@gmail.com>
2025-10-14 10:54:06 -0400

    Refactored initialize_data.py

--- data/scripts/initialize_data.py
+++ data/scripts/initialize_data.py
@@ -1,92 +1,30 @@
+import json
+import os
+import shutil
+import sys
 from pathlib import Path
-import json, os, requests, shutil, sys
 
-def fetch_data(parent_directory: str, raw_path: str, bestiary_path: str, rebuild: bool) -> None:
-    """ Fetches each of the files from GitHub and stores them in the raw_data folder."""
+import requests
+from github import Github
 
-    # TODO: Find way to fetch files from folder on github automatically
-    base_url = "https://raw.githubusercontent.com/foundryvtt/pf2e/refs/heads/v13-dev/packs/menace-under-otari-bestiary/"
-    filenames = [
-        "animated-armor-bb.json",
-        "basilisk-bb.json",
-        "boar-bb.json",
-        "brine-shark-bb.json",
-        "bugbear-prowler-bb.json",
-        "caligni-dancer-bb.json",
-        "caligni-hunter-bb.json",
-        "caligni-skulker-bb.json",
-        "cinder-rat-bb.json",
-        "doppelganger-bb.json",
-        "drow-priestess-bb.json",
-        "drow-sneak-bb.json",
-        "drow-warrior-bb.json",
-        "forest-troll-bb.json",
-        "gargoyle-bb.json",
-        "ghost-commoner-bb.json",
-        "ghoul-stalker-bb.json",
-        "giant-ant-bb.json",
-        "giant-centipede-bb.json",
-        "giant-rat-bb.json",
-        "giant-spider-bb.json",
-        "giant-viper-bb.json",
-        "goblin-commando-bb.json",
-        "goblin-igniter-bb.json",
-        "goblin-warrior-bb.json",
-        "harpy-bb.json",
-        "hell-hound-bb.json",
-        "hobgoblin-soldier-bb.json",
-        "horned-dragon-juvenile-bb.json",
-        "kobold-boss-zolgran-bb.json",
-        "kobold-dragon-mage-black-bb.json",
-        "kobold-dragon-mage-blue-bb.json",
-        "kobold-dragon-mage-green-bb.json",
-        "kobold-dragon-mage-red-bb.json",
-        "kobold-scout-bb.json",
-        "kobold-trapmaster-bb.json",
-        "kobold-warrior-bb.json",
-        "leopard-bb.json",
-        "mimic-bb.json",
-        "minotaur-hunter-bb.json",
-        "ogre-warrior-bb.json",
-        "orc-commander-bb.json",
-        "orc-scrapper-bb.json",
-        "orc-veteran-bb.json",
-        "owlbear-bb.json",
-        "pugwampi-bb.json",
-        "reefclaw-bb.json",
-        "sewer-ooze-bb.json",
-        "shadow-bb.json",
-        "skeletal-giant-bb.json",
-        "skeleton-guard-bb.json",
-        "sod-hound-bb.json",
-        "viper-bb.json",
-        "warg-bb.json",
-        "web-lurker-bb.json",
-        "wight-bb.json",
-        "wolf-bb.json",
-        "xulgath-leader-bb.json",
-        "xulgath-warrior-bb.json",
-        "zephyr-hawk-bb.json",
-        "zombie-shambler-bb.json"
-    ]
-    if not (Path(raw_path).exists()):
-        Path(raw_path).mkdir()
+
+def fetch_data(raw_path: str, bestiary_path: str, rebuild: bool) -> None:
+    """Fetches each of the files from GitHub and stores them in the raw_data folder"""  # noqa
+    check_dir(raw_path)
+
+    base_url = "https://raw.githubusercontent.com/foundryvtt/pf2e/refs/heads/v13-dev/packs/menace-under-otari-bestiary/"  # noqa
+    # Getting the files with PyGithub directly takes too long and results in
+    # being rate-limited, so just get the filenames with it
+    filenames = get_filenames()
 
     print("Downloading raw files...")
     for filename in filenames:
-        save_path = f"{raw_path}{filename}"
-        path = Path(save_path)
-        if not rebuild:
-            reformatted_file = filename.replace("-bb", "")
-            if Path(f"{bestiary_path}{reformatted_file}").exists():
-                print(f"{reformatted_file} already exists, skipping {filename} \n")
-                continue
+        if check_skip(rebuild, bestiary_path, filename):
+            continue
 
-            if path.exists():
-                print(f"{filename} already exists, skipping {filename} \n")
-                continue
-            
+        path = Path(f"{raw_path}{filename}")
         url = f"{base_url}{filename}"
+
         print(f"Requesting {filename}...")
         r = requests.get(url)
         print(f"Writing {filename}...")
@@ -95,137 +33,220 @@ def fetch_data(parent_directory: str, raw_path: str, bestiary_path: str, rebuild
 
     print("Raw files downloaded\n")
 
-def convert_data(parent_directory: str, raw_path: str, bestiary_path: str, rebuild: bool) -> None:
-    if not (Path(bestiary_path).exists()):
-        Path(bestiary_path).mkdir()
-    
+
+def convert_data(raw_path: str, bestiary_path: str, rebuild: bool) -> None:
+    """Convert each file in raw_data to a format that works better for Trailmarker"""  # noqa
+    errors_found = False
+    error_list = []
+
+    check_dir(bestiary_path)
+
     files = os.listdir(raw_path)
     for file in files:
-        reformatted_file = file.replace("-bb", "")
-        if not rebuild:
-            if Path(f"{bestiary_path}{reformatted_file}").exists():
-                continue
+        if check_skip(rebuild, bestiary_path, file):
+            continue
 
         path = Path(f"{raw_path}{file}")
         print(f"Loading {file}...")
         try:
             raw_dict = json.loads(path.read_text())
-        except (json.decoder.JSONDecodeError):
-            print(f"ERROR: could not decode {file}")
+        except json.decoder.JSONDecodeError:
+            print(f"ERROR: could not decode {file}.")
+            errors_found = True
+            error_list.append(file)
             continue
-        
+
         print(f"Reformatting {file}...")
-        monster = {
-                "name": "",
-                "level": 0,
-                "traits": [],
-                "perception": 0,
-                "skills": {},
-                "attributeModifiers": {
-                    "str": 0,
-                    "dex": 0,
-                    "con": 0,
-                    "int": 0,
-                    "wis": 0,
-                    "cha": 0
-                },
-                "defenses": {
-                    "armorClass": 10,
-                    "saves": {
-                        "fortitude": 0,
-                        "reflex": 0,
-                        "will": 0
-                    },
-                },
-                "maxHitPoints": 10,
-                "immunities": [],
-                "speed": 20,
-                "actions": {
-                    "attacks": {}
-                }
-            }
-        monster['name'] = raw_dict['prototypeToken']['name']
-        monster['level'] = raw_dict['system']['details']['level']['value']
+        enemy = build_enemy_dict(raw_dict)
 
-        if raw_dict['system']['traits']['rarity'] != 'common':
-            monster['traits'].append(raw_dict['system']['traits']['rarity'])
-        monster['traits'].append(raw_dict['system']['traits']['size']['value'])
-
-        traits = raw_dict['system']['traits']['value']
-        for trait in traits:
-            monster['traits'].append(trait)
-
-        try:
-            monster['perception'] = raw_dict['system']['perception']['mod']
-        except (KeyError):
-            print(f"No perception found in {monster['name']}. May be a hazard.")
-            continue
-
-        skills = raw_dict['system']['skills']
-        for skill in skills:
-            monster['skills'][skill] = skills[skill]['base']
-
-        abilities = raw_dict['system']['abilities']
-        for ability in abilities:
-          monster['attributeModifiers'][ability] = abilities[ability]['mod']
-        
-        system_attributes = raw_dict['system']['attributes']
-        monster['defenses']['armorClass'] = system_attributes['ac']['value']
-        saves = raw_dict['system']['saves']
-        for save in saves:
-            monster['defenses']['saves'][save] = saves[save]['value']
-
-        
-        monster['maxHitPoints'] = system_attributes['hp']['max']
-        monster['speed'] = system_attributes['speed']['value']
-        if "immunities" in system_attributes:
-            immunities = system_attributes['immunities']
-
-            for immunity in immunities:
-                monster['immunities'].append(immunity['type'])
-
-        items = raw_dict['items']
-
-        for item in items:
-            if "attack" in item['system']:
-                attack_dict = {}
-                attack_dict['attackBonus'] = item['system']['bonus']['value']
-                # for some reason in the JSON files from foundryVTT there is a
-                # key with a random value between damageRolls and the values
-                # inside it that I need, thus the damage_roll_key variable
-                try:
-                    damage_roll_key = list(item['system']['damageRolls'].keys())[0]
-                    attack_dict['damage'] = item['system']['damageRolls'][damage_roll_key]['damage']
-                    attack_dict['damageType'] = item['system']['damageRolls'][damage_roll_key]['damageType']
-                except (IndexError):
-                    pass
-                monster['actions']['attacks'].update({f"{item['name']}": attack_dict})
-
+        reformatted_file = file.replace("-bb", "")
         print(f"Saving {reformatted_file}...\n")
         path = Path(f"{bestiary_path}{reformatted_file}")
-        monster_json = json.dumps(monster, indent=4)
-        path.write_text(monster_json)
+        enemy_json = json.dumps(enemy, indent=4)
+        path.write_text(enemy_json)
+
+    if errors_found:
+        print("NOTE: The following files had errors:")
+        for file in error_list:
+            print(f"\t{file}")
 
     print("Files reformatted.\n")
 
-def cleanup(raw_path: str) -> None:
-    print("Cleaning up...")
-    shutil.rmtree(raw_path)
+
+def check_dir(dir: str) -> None:
+    """Creates needed directory if it doesn't exist"""
+    if not (Path(dir).exists()):
+        Path(dir).mkdir()
+
+
+def get_filenames() -> list[str]:
+    """Uses PyGithub to return a list of filenames retrieved from GitHub"""
+    g = Github()
+    repo = g.get_repo("foundryvtt/pf2e")
+    files = repo.get_contents("packs/menace-under-otari-bestiary")
+    exclusions = [
+        "central-spears-bb.json",
+        "envenomed-lock-bb.json",
+        "falling-ceiling-bb.json",
+        "hidden-pit-bb.json",
+        "mermaid-fountain-bb.json",
+        "scythe-blades-bb.json",
+        "slamming-door-bb.json",
+        "spear-launcher-bb.json",
+        "kobold-boss-zolgran.json",
+    ]
+    return [file.name for file in files if not (file.name in exclusions)]
+
+
+def check_skip(rebuild: bool, bestiary_path: str, filename: str) -> bool:
+    if rebuild:
+        return False
+
+    reformatted_file = filename.replace("-bb", "")
+    if Path(f"{bestiary_path}{reformatted_file}").exists():
+        print(f"{reformatted_file} already exists, skipping {filename}\n")
+        return True
+    else:
+        return False
+
+
+def build_enemy_dict(raw_dict: dict[any]) -> dict[any]:
+    enemy = {
+        "name": "",
+        "level": 0,
+        "traits": [],
+        "perception": 0,
+        "skills": {},
+        "attributeModifiers": {
+            "str": 0,
+            "dex": 0,
+            "con": 0,
+            "int": 0,
+            "wis": 0,
+            "cha": 0,
+        },
+        "defenses": {
+            "armorClass": 0,
+            "saves": {"fortitude": 0, "reflex": 0, "will": 0},
+        },
+        "maxHitPoints": 0,
+        "immunities": [],
+        "speed": 0,
+        "actions": {"attacks": {}},
+    }
+
+    enemy["name"] = raw_dict["prototypeToken"]["name"]
+    enemy["level"] = raw_dict["system"]["details"]["level"]["value"]
+
+    add_traits(raw_dict, enemy)
+
+    enemy["perception"] = raw_dict["system"]["perception"]["mod"]
+
+    add_skills(raw_dict, enemy)
+
+    add_attribute_modifiers(raw_dict, enemy)
+
+    system_attributes = raw_dict["system"]["attributes"]
+    enemy["defenses"]["armorClass"] = system_attributes["ac"]["value"]
+    add_saves(raw_dict, enemy)
+
+    enemy["maxHitPoints"] = system_attributes["hp"]["max"]
+    if "immunities" in system_attributes:
+        add_immunities(system_attributes, enemy)
+
+    enemy["speed"] = system_attributes["speed"]["value"]
+
+    add_attacks(raw_dict, enemy)
+
+    return enemy
+
+
+def add_traits(raw_dict: dict[any], enemy: dict[any]) -> None:
+    enemy["traits"].append(raw_dict["system"]["traits"]["rarity"])
+    enemy["traits"].append(raw_dict["system"]["traits"]["size"]["value"])
+
+    traits = raw_dict["system"]["traits"]["value"]
+    for trait in traits:
+        enemy["traits"].append(trait)
+
+
+def add_skills(raw_dict: dict[any], enemy: dict[any]) -> None:
+    skills = raw_dict["system"]["skills"]
+    for skill in skills:
+        enemy["skills"][skill] = skills[skill]["base"]
+
+
+def add_attribute_modifiers(raw_dict: dict[any], enemy: dict[any]) -> None:
+    abilities = raw_dict["system"]["abilities"]
+    for ability in abilities:
+        enemy["attributeModifiers"][ability] = abilities[ability]["mod"]
+
+
+def add_saves(raw_dict: dict[any], enemy: dict[any]) -> None:
+    saves = raw_dict["system"]["saves"]
+    for save in saves:
+        enemy["defenses"]["saves"][save] = saves[save]["value"]
+
+
+def add_immunities(system_attributes: dict[any], enemy: dict[any]):
+    immunities = system_attributes["immunities"]
+    for immunity in immunities:
+        enemy["immunities"].append(immunity["type"])
+
+
+def add_attacks(raw_dict, enemy):
+    items = raw_dict["items"]
+    for item in items:
+        if "attack" in item["system"]:
+            attack_dict = {}
+            attack_dict["attackBonus"] = item["system"]["bonus"]["value"]
+            # for some reason in the JSON files from foundryVTT there is a
+            # key with a random value between damageRolls and the values
+            # inside it that I need, thus the damage_roll_key variable
+            try:
+                damage_rolls = item["system"]["damageRolls"]
+                key = list(damage_rolls.keys())[0]
+                attack_dict["damage"] = damage_rolls[key]["damage"]
+                attack_dict["damageType"] = damage_rolls[key]["damageType"]
+            except IndexError:
+                pass  # Some attacks don't do damage, so don't add it
+            enemy["actions"]["attacks"].update({f"{item['name']}": attack_dict})  # noqa
+
+
+def usage_error() -> None:
+    usage = f"""Usage: python {sys.argv[0]} [options]
+            --rebuild\t rebuild data from scratch"""
+    print(usage)
+    exit()
+
 
 def main():
-    rebuild = False
     parent_directory = str(Path(__file__).parent.parent)
-    raw_path = f'{parent_directory}/raw_data/'
-    bestiary_path = f'{parent_directory}/bestiary/'
+    raw_path = f"{parent_directory}/raw_data/"
+    bestiary_path = f"{parent_directory}/bestiary/"
+
+    rebuild = False
 
     if len(sys.argv) == 2:
         if sys.argv[1] == "--rebuild":
             rebuild = True
+            if Path(f"{raw_path}").exists():
+                shutil.rmtree(raw_path)
+            if Path(f"{bestiary_path}").exists():
+                shutil.rmtree(bestiary_path)
+        else:
+            usage_error()
+    elif len(sys.argv) > 2:
+        usage_error()
 
-    fetch_data(parent_directory, raw_path, bestiary_path, rebuild)
-    convert_data(parent_directory, raw_path, bestiary_path, rebuild)
-    cleanup(raw_path)
+    fetch_data(raw_path, bestiary_path, rebuild)
+    convert_data(raw_path, bestiary_path, rebuild)
+
+    print("Cleaning up...")
+    shutil.rmtree(raw_path)
+
     print("Initialization complete!")
+
 
 if __name__ == "__main__":
     main()
