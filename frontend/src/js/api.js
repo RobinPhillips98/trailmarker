b7cac7303832ab51d2a0cd8d18e76721ea50e3c9
Robin Phillips <robinphillips462@gmail.com>
2026-02-26 16:41:31 -0500

    Updated token expiration time, added alert when token expires

--- frontend/src/js/api.js
+++ frontend/src/js/api.js
@@ -5,5 +5,24 @@ const api = axios.create({
   baseURL: "http://localhost:8000",
 });
 
+/**
+ * Sets up an Axios response interceptor that calls the provided logout
+ * function whenever a 401 Unauthorized response is received.
+ *
+ * @param {function} onUnauthorized - Callback to invoke on 401 responses
+ * @returns {number} The interceptor ID (can be used to eject it later)
+ */
+export function setupAuthInterceptor(onUnauthorized) {
+  return api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response?.status === 401) {
+        onUnauthorized();
+      }
+      return Promise.reject(error);
+    },
+  );
+}
+
 // Export the Axios instance
 export default api;
