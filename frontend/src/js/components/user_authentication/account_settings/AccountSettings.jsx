6374155fa8fcc4e4b1ef28c9d66ef30391b9c907
Robin Phillips <robinphillips462@gmail.com>
2026-03-07 16:12:58 -0500

    Frontend: Finished account settings page, can now change username and password

--- frontend/src/js/components/user_authentication/account_settings/AccountSettings.jsx
+++ frontend/src/js/components/user_authentication/account_settings/AccountSettings.jsx
@@ -1,11 +1,26 @@
+// Third-party libraries
+import { useContext } from "react";
 import { Tabs, Typography } from "antd";
 
+// Contexts
+import { AuthContext } from "../../../contexts/AuthContext";
+
+// Components
 import EditUsername from "./subcomponents/EditUsername";
 import EditPassword from "./subcomponents/EditPassword";
 import DeleteAccount from "./subcomponents/DeleteAccount";
+import NotAuthorized from "../../status_pages/NotAuthorized.jsx";
 
+/**
+ * A component to display account settings to the user.
+ *
+ * @returns {React.ReactElement}
+ */
 export default function AccountSettings() {
+  const { token } = useContext(AuthContext);
   const { Title } = Typography;
+
+  if (!token) return <NotAuthorized />;
 
   const items = [
     {
