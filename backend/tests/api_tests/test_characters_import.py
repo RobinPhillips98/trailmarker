ff2499c9ae9f481cd0fb933e931fe203bbd1dd73
Robin Phillips <robinphillips462@gmail.com>
2026-08-13 13:13:43 -0400

    Add unit tests for exception in imports endpoint

--- backend/tests/api_tests/test_characters_import.py
+++ backend/tests/api_tests/test_characters_import.py
@@ -1,5 +1,6 @@
 import json
 
+from tests.failures import sync_failure
 from tests.sample_imports import (
     import_cleric_data,
     import_fighter_data,
@@ -7,10 +8,12 @@ from tests.sample_imports import (
     import_wizard_data,
 )
 
+IMPORT_ROUTE = "/characters/import"
+
 
 def test_import_character_fighter(shared_auth_client, import_payload_fighter):
     response = shared_auth_client.post(
-        "/characters/import", json=import_payload_fighter
+        IMPORT_ROUTE, json=import_payload_fighter
     )
     assert response.status_code == 201
     created_character = response.json()
@@ -21,7 +24,7 @@ def test_import_character_fighter(shared_auth_client, import_payload_fighter):
 
 def test_import_character_cleric(shared_auth_client, import_payload_cleric):
     response = shared_auth_client.post(
-        "/characters/import", json=import_payload_cleric
+        IMPORT_ROUTE, json=import_payload_cleric
     )
     assert response.status_code == 201
     created_character = response.json()
@@ -33,9 +36,7 @@ def test_import_character_cleric(shared_auth_client, import_payload_cleric):
 
 
 def test_import_character_rogue(shared_auth_client, import_payload_rogue):
-    response = shared_auth_client.post(
-        "/characters/import", json=import_payload_rogue
-    )
+    response = shared_auth_client.post(IMPORT_ROUTE, json=import_payload_rogue)
     assert response.status_code == 201
     created_character = response.json()
     import_rogue_data.pop("user_id")
@@ -45,7 +46,7 @@ def test_import_character_rogue(shared_auth_client, import_payload_rogue):
 
 def test_import_character_wizard(shared_auth_client, import_payload_wizard):
     response = shared_auth_client.post(
-        "/characters/import", json=import_payload_wizard
+        IMPORT_ROUTE, json=import_payload_wizard
     )
     assert response.status_code == 201
     created_character = response.json()
@@ -58,3 +59,29 @@ def test_import_character_wizard(shared_auth_client, import_payload_wizard):
     print(json.dumps(created_character, indent=4))
     for key in import_wizard_data.keys():
         assert created_character[key] == import_wizard_data[key]
+
+
+def test_import_character_unauthenticated(client, import_payload_fighter):
+    response = client.post(IMPORT_ROUTE, json=import_payload_fighter)
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Could not validate credentials"
+
+
+def test_import_character_invalid(shared_auth_client):
+    request = {"name": "invalid"}
+    response = shared_auth_client.post(IMPORT_ROUTE, json=request)
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["msg"] == "Field required"
+
+
+def test_import_character_internal_error(
+    shared_auth_client, import_payload_fighter, monkeypatch
+):
+    monkeypatch.setattr(
+        "api.routes.characters.convert_to_db_character", sync_failure
+    )
+    response = shared_auth_client.post(
+        IMPORT_ROUTE, json=import_payload_fighter
+    )
+    assert response.status_code == 500
+    assert response.json()["detail"] == "Internal Server Error"
